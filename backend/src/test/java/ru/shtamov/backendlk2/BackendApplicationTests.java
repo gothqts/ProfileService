0081465fc02bce9ed7dbd7eb7eeb18981e53fe7b
Alexey <alexshamox@gmail.com>
2025-04-21 21:32:30 +0500

    init backend

---
+++ backend/src/test/java/ru/shtamov/backendlk2/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shtamov.backendlk2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
