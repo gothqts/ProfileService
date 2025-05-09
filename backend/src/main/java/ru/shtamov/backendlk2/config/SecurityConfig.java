26822d8fb607fb28ab51ce5c32b24c04a2230805
Alexey <alexshamox@gmail.com>
2025-05-09 09:42:23 +0500

    cors setting, jwt after registration

--- backend/src/main/java/ru/shtamov/backendlk2/config/SecurityConfig.java
+++ backend/src/main/java/ru/shtamov/backendlk2/config/SecurityConfig.java
@@ -104,7 +104,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5122"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
